9de4d8e31b69f91724a2d1faa2ce668a059fe368
ellienormal <elliespeller5@gmail.com>
2025-08-21 15:01:53 -0700

    adding autoscroll, scenery, character flipping
    zuzucube's FLIPPIN OUT

--- shmup/scenes/playertest.gd
+++ shmup/scenes/playertest.gd
@@ -3,12 +3,12 @@ extends CharacterBody2D
 #speed
 @export var speed: float = 400.0
 @export var focus_speed: float = 150.0
-
+@export var scroll_speed: float = 200
 #references to other nodes
 @onready var focus_sprite = $focusSprite
 @onready var fire_point = $firePoint
-@onready var beam = $beam
-
+@onready var beam = $beamArea
+@onready var sprite = $playerSprite
 #focus
 var focused: bool = false
 var type = "player" #for interaction with world objects
@@ -40,23 +40,26 @@ func handle_focus_mode():
 func handle_movement(delta):
 	var input_direction = Input.get_vector("left", "right", "up", "down") #get input direction as a vector2
 	var current_speed = focus_speed if focused else speed
-	velocity = input_direction * current_speed
+	velocity = Vector2(scroll_speed, 0) + input_direction * current_speed
 	
 
 func handle_rotation():
 	var mouse_pos = get_global_mouse_position()
-	
 	var direction = mouse_pos - global_position
+	
 	rotation = direction.angle()
+		# Flip sprite vertically when aiming left
+	if rotation > PI/2 or rotation < -PI/2:
+		sprite.flip_v = true
+	else:
+		sprite.flip_v = false
 
 func handle_beam(delta):
 	if Input.is_action_pressed("shoot"):
 		if beamCharge < .5:
 			beamCharge += delta
-			print("Charging...")
 		else:
 			beam.visible = true
-			print("Fire!")
 	if Input.is_action_just_released("shoot"):
 		beam.visible = false
 		beamCharge = 0
